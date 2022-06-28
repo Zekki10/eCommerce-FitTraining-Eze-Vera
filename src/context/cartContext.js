facb385295799a5820235cc8fa898485c24d1395
Zekki10 <ezequiel_vera_rodriguez@hotmail.com>
2022-06-28 01:33:02 -0300

    permanencia de datos del Carrito con LocalStorage

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -7,7 +7,7 @@ const cartContext = createContext()
 
 const CartProvider = ({children}) => {
 
-    const [cartListItems, setCartListItems] = useState([])
+    const [cartListItems, setCartListItems] = useState(JSON.parse(localStorage.getItem('products')) || [])
     const [itemQuantity, setItemQuantity ] = useState(0)
     const [totalPrice, setTotalPrice] = useState(0)
     const [itemProps, setItemProps] = useState([])
@@ -23,6 +23,10 @@ const CartProvider = ({children}) => {
         .then((response) => {
             setLoading(false)
             setItemProps(response)
+            if (JSON.parse(localStorage.getItem('products'))) {
+                setCartQuantity(JSON.parse(localStorage.getItem('itemQty')))
+                setTotalPrice(JSON.parse(localStorage.getItem('totalPrice')))
+            }
         })        
     }, [category])
 
@@ -34,15 +38,21 @@ const CartProvider = ({children}) => {
             setCartListItems(cartListItems=> [...cartListItems,product])
             setItemQuantity(quantity)
             setCartQuantity(cartQuantity + quantity)
+            localStorage.setItem('products', JSON.stringify([...cartListItems,product]))
+            localStorage.setItem('itemQty', JSON.stringify(cartQuantity + quantity))
+            localStorage.setItem('totalPrice', JSON.stringify(totalPrice+product.qty*product.price))
         } 
     }
     const removeItem = (id) => {
         const newCart = cartListItems.filter((product) => product.id !== id)
         setCartListItems(newCart);
+        localStorage.setItem('products', JSON.stringify(newCart))
         cartListItems.map((product) => {
             if (product.id === id) {
                 setTotalPrice(totalPrice - product.qty*product.price)
                 setCartQuantity(cartQuantity - product.qty)
+                localStorage.setItem('totalPrice', JSON.stringify(totalPrice - product.qty*product.price))
+                localStorage.setItem('itemQty', JSON.stringify(cartQuantity - product.qty))
             } return ''
         })
     }
@@ -50,6 +60,9 @@ const CartProvider = ({children}) => {
         setCartListItems([])
         setTotalPrice(0)
         setCartQuantity(0)
+        localStorage.setItem('products', JSON.stringify([]))
+        localStorage.setItem('itemQty', JSON.stringify(0))
+        localStorage.setItem('totalPrice', JSON.stringify(0))
     }
 
     const data = {
