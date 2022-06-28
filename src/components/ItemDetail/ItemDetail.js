f37d974bf81f984d42b08c1b6aad78fb998628ac
Zekki10 <ezequiel_vera_rodriguez@hotmail.com>
2022-06-28 11:36:23 -0300

    correccion de estilos

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,4 +1,4 @@
-import { Container, Grid } from "@mui/material"
+import { Box, Grid } from "@mui/material"
 import { useState, useEffect } from "react"
 import './ItemDetail.css'
 import Button from '@mui/material/Button';
@@ -38,46 +38,53 @@ export const ItemDetail = (props) => {
     
     if (load === false ) {
             return (
-                <Container style={{height:'100%',padding: '0 0 0 0' }} wrap={'wrap'} className="container_detail" > 
-                    <Grid container justifyContent="space-around" sx={{height:'100%',padding: '0px 0px 0px 0px'}} className='grid_container'> 
-                        <Grid item justifyContent="center" sx={{height:'100%',padding: '0px 0px 0px 0px'}} sm={7} md={8} className='product_container'> 
-                            {
-                                Object.values({items}).map( (item, index) => {
-                             
-                                    return (
-                                        <div className="imageContainer" key={index}>
-                                            <img className="mainPicture" src={`/${item.img}`} alt="img"></img>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </Grid>
-                        <Grid item className="desc_container" justifyContent="center" alignItems="center"  sm={5} md={4}>
-                                            
-                            {
-                                Object.values({items}).map( ({title, price, img, id}, index) =>{
-                                    return (
-                                        <div key={index} className="infoContainer">
-                                            <h1>{title}</h1>
-                                            <p className="description">Item description</p>
-                                            <div className="precio_container">
-                                                <span className="precio">{`$${price}`}</span>
-                                                <span className="cuotas">{`3 cuotas sin interes de $${Math.round(price/3)}`}</span>
-                                            </div>
-                                            { !showButton ? 
-                                            <ItemCount id={id} items={items} quantity={quantity} setQuantity={setQuantity} setShowButton={setShowButton}></ItemCount>
-                                            :
-                                            <Button variant="contained"  className='button_card'>
-                                                <Link className="link_buy" to='/cart'>Buy now</Link>
-                                            </Button>}
-                                        </div>
-                                    ) 
+                <>
+                    <Box sx={{ flexGrow: 1, 
+                        width:'100%', 
+                        padding:'10px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center' }} >
+                        <Grid container justifyContent="space-around" sx={{height:'100%',padding: '0px 0px 0px 0px'}} className='grid_container'> 
+                            <Grid item justifyContent="center" sx={{height:'100%',padding: '0px 0px 0px 0px'}} md={6} xs={12} className='product_container'> 
+                                {
+                                    Object.values({items}).map( (item, index) => {
                                 
-                                })
-                            }                        
+                                        return (
+                                            <div className="imageContainer" key={index}>
+                                                <img className="mainPicture" src={`/${item.img}`} alt="img"></img>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </Grid>
+                            <Grid item className="desc_container" justifyContent="center" alignItems="center"  md={6} xs={12}>
+                                                
+                                {
+                                    Object.values({items}).map( ({title, price, img, id}, index) =>{
+                                        return (
+                                            <div key={index} className="infoContainer">
+                                                <h1>{title}</h1>
+                                                <p className="description">Item description</p>
+                                                <div className="precio_container">
+                                                    <span className="precio">{`$${price}`}</span>
+                                                    <span className="cuotas">{`3 cuotas sin interes de $${Math.round(price/3)}`}</span>
+                                                </div>
+                                                { !showButton ? 
+                                                <ItemCount id={id} items={items} quantity={quantity} setQuantity={setQuantity} setShowButton={setShowButton}></ItemCount>
+                                                :
+                                                <Button variant="contained"  className='button_card'>
+                                                    <Link className="link_buy" to='/cart'>Buy now</Link>
+                                                </Button>}
+                                            </div>
+                                        ) 
+                                    
+                                    })
+                                }                        
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Container>
+                    </Box>
+                </>
             )
         } else {
         return (
